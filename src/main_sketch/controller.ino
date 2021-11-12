a7772fb40db5dd19eabe8ffd88437133103955e7
Ayush Ghosh <ayushghosh21@gmail.com>
2021-11-11 20:37:20 -0700

    Multi Directional Motors. Toggling between FORWARD and BACKWARD

--- src/main_sketch/controller.ino
+++ src/main_sketch/controller.ino
@@ -2,15 +2,20 @@ void controller(void *pvParameters)
 {
   (void) pvParameters;
 
-  int coutner = 0;
+  int coutner = -1;
   int stepper_msg_counter = 0;
   for (;;)
   {
     msg_union msg;
     msg.motor_message.type = MSG_MOTOR;
     msg.motor_message.timestamp = millis();
+    if (coutner == -1){
+      msg.motor_message.dir = FORWARD;
+      coutner++;  
+    }
+    
+    
     msg.motor_message.spd = coutner;
-    msg.motor_message.dir = FORWARD;
     msg.motor_message.step_dir = STEPPER_MOVE_NONE;
 
     if (stepper_msg_counter == 70){
@@ -22,10 +27,21 @@ void controller(void *pvParameters)
     stepper_msg_counter++;
     
     coutner++;
-    if (coutner > 40){
-      msg.motor_message.spd = 80 - coutner;
+    if (coutner > 80){
+      msg.motor_message.spd = 160 - coutner;
     }
-    coutner%=80;
+    coutner%=160;
+    
+    if (coutner == 0) {
+      if (msg.motor_message.dir == FORWARD){
+        msg.motor_message.dir = BACKWARD;
+        
+      }
+      else if (msg.motor_message.dir == BACKWARD){
+        msg.motor_message.dir = FORWARD;
+        Serial.println("Set to FORWARD-------------------------");
+      }
+    }
     
     Serial.println(msg.motor_message.step_dir);
     xQueueSend(actuators_Mailbox,&msg,portMAX_DELAY);
