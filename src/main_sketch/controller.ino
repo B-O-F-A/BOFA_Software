69164c8a991ec0aec5e12ffefa53bbc3a2c229a7
Ayush Ghosh <ayushghosh21@gmail.com>
2021-11-16 13:01:06 -0700

    FOR DEMO DEMO

--- src/main_sketch/controller.ino
+++ src/main_sketch/controller.ino
@@ -4,15 +4,36 @@ void controller(void *pvParameters)
 
   int coutner = 0;
   int stepper_msg_counter = 0;
+  int motor_speed = 80;
+  
   for (;;)
   {
+    
     msg_union msg;
     msg.motor_message.type = MSG_MOTOR;
     msg.motor_message.timestamp = millis();
-    msg.motor_message.spd = coutner;
-    msg.motor_message.dir = FORWARD;
     msg.motor_message.step_dir = STEPPER_MOVE_NONE;
 
+    if (coutner == 10){
+      motor_speed = 65;
+    }
+
+    if (coutner == 20){
+      msg.motor_message.step_dir = CLOSE_GATE;
+    }
+
+    if (coutner == 30){
+      motor_speed = 80;
+    }
+
+    if (coutner == 40){
+      motor_speed = 0;
+    }
+    
+    msg.motor_message.spd = motor_speed;
+    msg.motor_message.dir = FORWARD;
+
+    /*
     if (stepper_msg_counter == 70){
       msg.motor_message.step_dir = CLOSE_GATE;
     }else if (stepper_msg_counter == 140){
@@ -20,14 +41,10 @@ void controller(void *pvParameters)
       stepper_msg_counter = -1;
     }
     stepper_msg_counter++;
-    
-    coutner++;
-    if (coutner > 40){
-      msg.motor_message.spd = 80 - coutner;
-    }
-    coutner%=80;
-    
-    Serial.println(msg.motor_message.step_dir);
+    */
+
+    counter ++;
+    //Serial.println(msg.motor_message.step_dir);
     xQueueSend(actuators_Mailbox,&msg,portMAX_DELAY);
     vTaskDelay(pdMS_TO_TICKS(200));
   }
